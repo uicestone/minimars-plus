d01fdf1ad745c65345c26b20153218a40d680c85
Uice Lu <uicestone@gmail.com>
2021-04-25 22:11:26 +0800

    compress image; remove dummy code

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,12 @@
 export default {
   mallAppId: "wx28602891799cb16e",
   apiBaseUrl: "http://localhost:8001/api",
+  bookingStatusName: {
+    pending: "待付款",
+    booked: "已确认",
+    in_service: "已入场",
+    pending_refund: "待撤销",
+    finished: "已完成",
+    canceled: "已取消",
+  },
 };
